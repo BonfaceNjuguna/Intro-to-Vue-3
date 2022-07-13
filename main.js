dd65bd31784c1f996a30c3710490b953f919e233
Bonface Njuguna <bonfacenjuguna13@gmail.com>
2022-07-13 20:35:18 +0300

    added in stock condition

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            url: 'https://learninglions.org/img/basic_training_icon_mouseover.png'
+            inventory: 0
         }
     }
 })
