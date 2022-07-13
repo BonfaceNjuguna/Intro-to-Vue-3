42e6eba5d9e446d28a54d36fc56a3e4e28666038
Bonface Njuguna <bonfacenjuguna13@gmail.com>
2022-07-13 20:10:32 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://learninglions.org/img/basic_training_icon_mouseover.png'
         }
     }
 })
